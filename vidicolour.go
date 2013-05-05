ec89ab47f23f47e0f0d3f41b49fb71b453cca50e
Gordan Grasarevic <me@ggordan.com>
2013-05-05 17:08:04 +0100

    Getting image RGBA values

--- vidicolour.go
+++ vidicolour.go
@@ -3,19 +3,43 @@ package main
 import (
 	"code.google.com/p/go-opencv/trunk/opencv"
 	"fmt"
+	"image/png"
+	"log"
 	"os"
 )
 
 func saveImages(img_path string, img *opencv.IplImage, fpm int) {
 	opencv.SaveImage(img_path, img, 0)
 	opencv.WaitKey(fpm)
+	colourSample(img_path)
+}
+
+func colourSample(path string) {
+	immg, err := os.Open(path)
+	defer immg.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	decoded_image, _ := png.Decode(immg)
+	max_y := decoded_image.Bounds().Max.Y
+	max_x := decoded_image.Bounds().Max.X
+
+	go func() {
+		for i := 0; i <= max_y; i++ {
+			for y := 0; y <= max_x; y++ {
+				log.Printf("%v \n", decoded_image.At(max_x-y, max_y-i))
+			}
+		}
+	}()
 }
 
 func main() {
 
 	img_path := "sample_images/"
 
-	filename := nil
+	filename := "movie.mkv"
 	if len(os.Args) == 2 {
 		filename = os.Args[1]
 	} else {
@@ -28,8 +52,6 @@ func main() {
 		panic("can not open video")
 	}
 	defer cap.Release()
-
-	fmt.Printf("%s", "Adasdasdsa")
 
 	// Get an image every 5 minutes
 	fpm := int(cap.GetProperty(opencv.CV_CAP_PROP_FPS) * (60 * 5))
